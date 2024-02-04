1c336a792f251e1f4bca046885d94ded36d33d6a
NayosVieurer <manuminardi@orange.fr>
2024-02-04 00:54:36 +0000

    removed useless log

--- Channels/sampleChannel.js
+++ Channels/sampleChannel.js
@@ -3,7 +3,5 @@ exports.channelId = "1203496044340580502";
 
 exports.run = (client, message, args) =>
 {
-  console.log(message.channel);
-
   message.channel.send("this is a reply to a message in a specific channel");
 };
