ba3ad82ab6621ff8b9ce99d1227f5b20d6003dd8
NayosVieurer <manuminardi@orange.fr>
2024-02-04 00:40:17 +0000

    fixing couple of issues with the core system

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-const cron = require('node-cron');
 const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -6,7 +5,7 @@ const config = require('./config.json');
 const utils = require("./Utils.js");
 const db = require("./database.js");
 
-let remap = {};
+let remap = {commands:[], channels:[]};
 
 function refreshRemap()
 {
@@ -26,7 +25,7 @@ function refreshRemap()
         {
           list.forEach(commandId =>
           {
-            remap[commandId] = file;
+            remap.commands[commandId] = file;
           });
         });
       });
@@ -41,7 +40,7 @@ function refreshRemap()
       files.forEach(file =>
       {
           let channelFile = require("./Channels/" + file);
-          remap[channelFile.channelId] = file;
+          remap.channels[channelFile.channelId] = file;
       });
     });
   });
@@ -52,7 +51,7 @@ function handleCommand(message)
   let fullCommand = message.content.slice(config.prefix.length).trim().split("-");
   let commandName = fullCommand.shift().trim().replace(/ +/g, "_").toLowerCase();
 
-  let commandFileName = remap[commandName];
+  let commandFileName = remap.commands[commandName];
 
   let args = fullCommand.length > 0 ? fullCommand.shift().split(/ +/g) : [];
 
@@ -78,7 +77,6 @@ client.on("ready", async () => {
   refreshRemap();
 
   db.dbConnection();
-
   // This event will run if the bot starts, and logs in, successfully.
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds. \n `);
   // Example of changing the bot's playing game to something useful. `client.user` is what the
@@ -92,12 +90,14 @@ client.on("message", message => {
 	if (message.author.bot || config.blacklist.includes(message.author.id))
     return;
 
-  if(config.SpecialChannels.includes(message.channel.id))
+  if(remap.channels[message.channel.id])
   {
-    let channelFile = require("./Channels/" + remap[message.channel.id]);
-    channelFile.run(message, client)
+    let channelFile = require("./Channels/" + remap.channels[message.channel.id]);
+    channelFile.run(client, message)
     return;
   }
+
+
 
   if(message.content.indexOf(config.prefix) != 0)
     return;
